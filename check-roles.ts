5b0641940e026a7ca232768ebf3d03ffb82ceedf
FritzKialo <91533967+FritzKialo@users.noreply.github.com>
2026-02-05 07:14:57 +0300

    Update check-roles.ts
    Signed-off-by: FritzKialo <91533967+FritzKialo@users.noreply.github.com>

--- check-roles.ts
+++ check-roles.ts
@@ -3,7 +3,7 @@ import { db } from "./lib/db";
 async function main() {
     const users = await db.user.findMany();
     console.log("Registered Users:");
-    users.forEach((u) => {
+    users.forEach((u: any) => {
         console.log(`- ${u.email}: ${u.role}`);
     });
 }
